f854ed4361536d88d4614a6228ff78f3bba76aef
escalasebastian <escalasebastian@gmail.com>
2024-02-15 13:17:33 +0100

    Creación modelos, ruta y migraciones

--- TriniAnim/database/migrations/2024_02_15_115621_create_eventos_table.php
+++ TriniAnim/database/migrations/2024_02_15_115621_create_eventos_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('eventos', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            //$table->string('email')->unique();
-            //$table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('usuario_id');
+            $table->integer('actividad_id');
+            $table->integer('emocion_id');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('eventos');
     }
 };
