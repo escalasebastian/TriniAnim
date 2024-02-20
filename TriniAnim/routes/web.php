3581c48fd8d7780125cbab2e4794c09a6df0a7b7
albvn <id00686127@usal.es>
2024-02-20 01:11:49 +0100

    REQ-AUT-190224. Incorporacion de autenticacion mediante breeze (Adapptada al proyecto v1.0)

--- TriniAnim/routes/web.php
+++ TriniAnim/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TriniAnimController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::resource('noticias', NoticiasController::class);
-
-Route::resource('trinianim', TriniAnimController::class);
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/dashboard', function () {
+    return view('pruebaIndex');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
