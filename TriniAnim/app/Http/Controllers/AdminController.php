67aac52b924b702682e7e225b53d91b5247843c3
escalasebastian <escalasebastian@gmail.com>
2024-02-27 08:48:55 +0100

    REQ-Script-260224 - Creación gráfico, arreglo js que dañe en el commit anterior

--- TriniAnim/app/Http/Controllers/AdminController.php
+++ TriniAnim/app/Http/Controllers/AdminController.php
@@ -14,9 +14,52 @@ class AdminController extends Controller
 
     public function indexAdmin()
     {
+        // Emociones
+        $emociones = Emocion::all();
+        $arrayEmociones = array();
+        foreach ($emociones as $emocion) {
+            array_push($arrayEmociones, $emocion->emocion); // emoción es el nombre de la emoción
+        }
+        $arrayEmocionesJs = json_encode($arrayEmociones);
+
+        // Eventos
+        $eventos = Evento::all();
+        $contEnfadado = 0;
+        $contTriste = 0;
+        $contNormal = 0;
+        $contContento = 0;
+        $contEuforico = 0;
+        foreach ($eventos as $evento) {
+            switch ($evento->emocion_id) {
+                case 1:
+                    $contEnfadado++;
+                    break;
+                case 2:
+                    $contTriste++;
+                    break;
+                case 3:
+                    $contNormal++;
+                    break;
+                case 4:
+                    $contContento++;
+                    break;
+                case 5:
+                    $contEuforico++;
+                    break;
+                default:
+                    # code...
+                    break;
+            }
+        }
+        $arrayEventosValues = array($contEnfadado, $contTriste, $contNormal, $contContento, $contEuforico);
+        $arrayEventosValuesJs = json_encode($arrayEventosValues);
+
+        // Usuarios
         $usuarios = User::whereNotIn('userName', ['admin'])->get();
         return view('trini.admin', [
-            'usuarios' => $usuarios
+            'usuarios' => $usuarios,
+            'emociones' => $arrayEmocionesJs,
+            'eventos' => $arrayEventosValuesJs
         ]);
         // $usuarios=User::all();
         // return view('trini.admin', ['usuarios' => $usuarios]);
